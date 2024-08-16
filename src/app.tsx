96dc745bdc90ff0c817179b429c70dfbf6cf19c9
Matt Crowder <matt.crowder@veeva.com>
2024-08-15 19:34:40 -0500

    add displaying count plus one

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,11 @@ import { useState, useEffect } from "react"
 
 function App() {
   const [count, setCount] = useState(0)
+  const [countPlusOne, setCountPlusOne] = useState(0)
 
+  useEffect(() => {
+    setCountPlusOne(count + 1)
+  }, [count])
   useEffect(() => {
     if (count === 5) {
       console.log("count is ", count)
@@ -12,6 +16,7 @@ function App() {
     <div>
       <button onClick={() => setCount(count + 1)}>+1</button>
       <p>{String(count)}</p>
+      <p>{String(countPlusOne)}</p>
       <button onClick={() => setCount(count - 1)}>-1</button>
     </div>
   )
